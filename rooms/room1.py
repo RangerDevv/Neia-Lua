ce5896c4307a93b58307dd08fcbb7f32c90a158a
Rangerdevv <meerabdul2008@gmail.com>
2022-04-02 14:33:07 -0400

    added plaer

--- rooms/room1.py
+++ rooms/room1.py
@@ -1,5 +1,3 @@
-from tkinter import font
-from pip import main
 import pygame
 import sys
 import os
@@ -14,15 +12,9 @@ background_color = (0, 0, 0)
 font = pygame.font.SysFont(None, 25)
 
 def room1():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    while True:
-        screen.fill(background_color)
-        shared.Textvar = "You have spawned in the prison cell. You dont really know how you ended up here, but you know that you have to get out of here. Press [ENTER] to continue."
-        text = font.render(shared.Textvar, True, (255, 255, 255))
-        screen.blit(text, ( shared.screen_width/2 , shared.screen_height/2 ))
-        pygame.display.update()
-        if pygame.event == pygame.K_RETURN:
-            shared.Textvar = "There are 4 directions you can look in. Press [W] to look up, [A] to look left, [S] to look down, [D] to look right. Press [ENTER] to continue."
+    while True: # main game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    
