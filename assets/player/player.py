1acdd0e083a897477a69a6f30b80b973fcf61a06
Rangerdevv <meerabdul2008@gmail.com>
2022-04-02 14:34:52 -0400

    added player

---
+++ assets/player/player.py
@@ -0,0 +1,11 @@
+# make a player object
+player = {
+    "name": "",
+    "health": 100,
+    "maxhealth": 100,
+    "damage": 10,
+    "defense": 10,
+    "armor": 5,
+    "gold": 0,
+    "inventory": [],
+}
